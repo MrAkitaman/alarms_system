f1435d3b9bb733aa369795fb3b471a854e1e7c85
Paul <59295623+paulfuriox@users.noreply.github.com>
2023-02-02 20:23:30 +0100

    indentation correction

--- lua/scppalarms_folder/server/nets.lua
+++ lua/scppalarms_folder/server/nets.lua
@@ -3,9 +3,11 @@ util.AddNetworkString( "SCPAlarms::NetComm" )
 local tNets = {
     [1] = function( pPly )
         if not pPly:IsAdmin() then return end
+        
         local iCat = net.ReadUInt(8)
         local iSong = net.ReadUInt(16)
         local bLoop = net.ReadBool()
+        
         net.Start( "SCPAlarms::NetComm" )
         net.WriteUInt( 2, 4 )
         net.WriteUInt( iCat, 8 )
@@ -15,6 +17,7 @@ local tNets = {
     end,
     [2] = function( pPly )
         if not pPly:IsAdmin() then return end
+        
         net.Start( "SCPAlarms::NetComm" )
         net.WriteUInt( 3, 4 )
         net.Broadcast()
@@ -23,6 +26,7 @@ local tNets = {
 
 net.Receive( "SCPAlarms::NetComm", function( _, pPly )
     local iKey = net.ReadUInt( 4 )
+        
     if not ( IsValid( pPly ) and tNets[iKey] and isfunction( tNets[iKey] ) ) then return end
     tNets[iKey]( pPly )
 end )
