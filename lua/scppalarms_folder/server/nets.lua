b2b8dc2dbad1d30f4f28392c51626ad49c55a262
MrAkitaman <36459527+MrAkitaman@users.noreply.github.com>
2023-03-29 20:30:08 +0200

    Refacto

--- lua/scppalarms_folder/server/nets.lua
+++ lua/scppalarms_folder/server/nets.lua
@@ -1,4 +1,4 @@
-util.AddNetworkString( "SCPAlarms::NetComm" )
+util.AddNetworkString("SCPAlarms::NetComm")
 
 local tNets = {
     [1] = function( pPly )
@@ -8,25 +8,25 @@ local tNets = {
         local iSong = net.ReadUInt(16)
         local bLoop = net.ReadBool()
         
-        net.Start( "SCPAlarms::NetComm" )
-        net.WriteUInt( 2, 4 )
-        net.WriteUInt( iCat, 8 )
-        net.WriteUInt( iSong, 16 )
-        net.WriteBool( bLoop )
+        net.Start("SCPAlarms::NetComm")
+        net.WriteUInt(2, 4)
+        net.WriteUInt(iCat, 8)
+        net.WriteUInt(iSong, 16)
+        net.WriteBool(bLoop)
         net.Broadcast()
     end,
     [2] = function( pPly )
         if not pPly:IsAdmin() then return end
         
-        net.Start( "SCPAlarms::NetComm" )
-        net.WriteUInt( 3, 4 )
+        net.Start("SCPAlarms::NetComm")
+        net.WriteUInt(3, 4)
         net.Broadcast()
     end,
 }
 
-net.Receive( "SCPAlarms::NetComm", function( _, pPly )
-    local iKey = net.ReadUInt( 4 )
+net.Receive("SCPAlarms::NetComm", function(_, pPly)
+    local iKey = net.ReadUInt(4)
         
-    if not ( IsValid( pPly ) and tNets[iKey] and isfunction( tNets[iKey] ) ) then return end
-    tNets[iKey]( pPly )
+    if not (IsValid(pPly) and tNets[iKey] and isfunction(tNets[iKey])) then return end
+    tNets[iKey](pPly)
 end )
